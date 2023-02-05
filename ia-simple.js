df0ba2f4773d4f073a5027c3d2e9f50014ac46ce
Liamcr <cariou.liam@orange.fr>
2023-02-05 23:30:25 +0100

    start

--- ia-simple.js
+++ ia-simple.js
@@ -11,3 +11,29 @@ const startGame = () => {
   currentTurn = playerSymbol;
   winner = null;
 
+  const table = document.querySelector('#gameBoard');
+  table.innerHTML = '';
+
+  for (let i = 0; i < gridSize; i++) {
+    const row = document.createElement('tr');
+    for (let j = 0; j < gridSize; j++) {
+      const cell = document.createElement('td');
+      cell.addEventListener('click', () => {
+        if (!winner && gameBoard[i][j] === null) {
+          cell.textContent = playerSymbol;
+          gameBoard[i][j] = playerSymbol;
+          checkForWinningConditions();
+          if (!winner) {
+            const aiMove = getAIMove();
+            gameBoard[aiMove.i][aiMove.j] = getAISymbol();
+            document.querySelector(`tr:nth-child(${aiMove.i + 1}) td:nth-child(${aiMove.j + 1})`).textContent = getAISymbol();
+            checkForWinningConditions();
+            }
+            }
+            });
+            row.appendChild(cell);
+            }
+            table.appendChild(row);
+            }
+            };
+  
