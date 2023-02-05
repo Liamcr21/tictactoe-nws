b60bd88a6a68cd82f86707edf0c53f7e735a6c8c
Liamcr <cariou.liam@orange.fr>
2023-02-06 00:15:47 +0100

    init js

---
+++ ia-imbatable.js
@@ -0,0 +1,21 @@
+let TableDeJeu;
+
+// joueurs
+const humain = '0';
+const ai = 'x';
+
+// Combinaison gagnante
+const winCombo = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+
