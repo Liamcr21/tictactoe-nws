60ec1d2a5826fdbcc06d5e3641ac2fc76aac1e64
Liamcr <cariou.liam@orange.fr>
2023-02-05 23:29:32 +0100

    debut fonction start

--- ia-simple.js
+++ ia-simple.js
@@ -4,3 +4,10 @@ let gameBoard;
 let currentTurn;
 let winner;
 
+const startGame = () => {
+  gridSize = parseInt(document.querySelector('input[name="gridSize"]:checked').value);
+  playerSymbol = document.querySelector('input[name="symbol"]:checked').value;
+  gameBoard = Array(gridSize).fill(null).map(() => Array(gridSize).fill(null));
+  currentTurn = playerSymbol;
+  winner = null;
+
